b8228c65d1481e1c2e730d093d6141dd503b84e8
luifee <lynnyuhuang@gmail.com>
2021-06-09 14:24:46 +0000

    2.12_plus_i18n_zh-TW

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Citiesocial
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Asia/Taipei'
+    config.i18n.default_locale = :"zh-TW"
 
     # Configuration for the application, engines, and railties goes here.
     #
