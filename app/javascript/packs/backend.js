d8a69da3ee89e9c706ee561be30f27ea364026f6
luifee <lynnyuhuang@gmail.com>
2021-06-19 07:17:04 +0000

    2.23however

--- app/javascript/packs/backend.js
+++ app/javascript/packs/backend.js
@@ -1,10 +1,6 @@
-// import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-// import * as ActiveStorage from "@rails/activestorage"
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
 
 // enable action text
 require("trix")
