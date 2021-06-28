74ae5513d0120c9df448040df13ec56d9d184351
luifee <lynnyuhuang@gmail.com>
2021-06-28 09:00:24 +0000

    2.28

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'products#index'
+
   resources :products, only: [:index, :show]
 
   namespace :admin do
     root 'products#index'
     resources :products, except: [:show]
     resources :vendors, except: [:show]
+  end
+
+  namespace :api do
+    namespace :v1 do
+      post 'subscribe', to: 'utils#subscribe'
+    end
   end
 
 end
