4e996cecc8c12f19a0934075acf61b59a48017f1
luifee <lynnyuhuang@gmail.com>
2021-06-29 08:30:09 +0000

    2.31

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'omniauth-rails_csrf_protection', '~> 1.0'
 gem 'figaro', '~> 1.2'
 gem 'paranoia', '~> 2.4', '>= 2.4.3'
 gem 'friendly_id', '~> 5.4', '>= 5.4.2'
+gem 'acts_as_list', '~> 1.0', '>= 1.0.4'
 
 gem 'mimemagic', github: 'mimemagicrb/mimemagic', ref: '01f92d86d15d85cfd0f20dabd025dcbd36a8a60f'
 
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,35 @@
 class Admin::CategoriesController < Admin::BaseController
 
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: '成功新增分類'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
